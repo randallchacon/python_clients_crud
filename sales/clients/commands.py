7a6585477d9dba239425353bc17c2de900207fe0
randallchacon <info@randallchacon.com>
2023-01-18 18:39:43 -0600

    Adding create and update methods to command and services files

--- sales/clients/commands.py
+++ sales/clients/commands.py
@@ -1,3 +1,4 @@
+#source venv/bin/activate  ->VM
 import click
 
 from clients.services import ClientService
@@ -40,13 +41,53 @@ def create(ctx, name, company, email, position):
 @click.pass_context
 def list(ctx):
     """List all clients"""
+    client_service = ClientService(ctx.obj['clients_table'])
+
+    client_list = client_service.list_clients()
+
+    click.echo('ID | NAME | COMPANY | EMAIL | POSITION')
+    click.echo('*'*100)
+
+    for client in client_list:
+        click.echo('{uid} | {name} |{company} | {email} | {position}'.format(
+            uid=client['uid'],
+            name=client['name'],
+            company=client['company'],
+            email=client['email'],
+            position=client['position'])
+        )        
 
 
 @clients.command()
+@click.argument('client_uid',
+                type=str)
 @click.pass_context
 def update(ctx, client_uid):
     """Updates a client"""
-    pass
+    client_service = ClientService(ctx.obj['clients_table'])
+    
+    client_list = client_service.list_clients()
+
+    client = [client for client in client_list if client['uid'] == client_uid]
+
+    if client:
+        client = _updated_client_flow(Client(**client[0])) #extract client of the list
+        client_service.update_client(client)
+
+        click.echo('Client updated')
+    else:
+        click.echo('Client not found')
+
+
+def _updated_client_flow(client):
+    click.echo('Leave empty if you dont want to modify the value')
+
+    client.name = click.prompt('New name', type=str, default=client.name)
+    client.company = click.prompt('New company', type=str, default=client.company)
+    client.email = click.prompt('New email', type=str, default=client.email)
+    client.position = click.prompt('New position', type=str, default=client.position)
+
+    return client
 
 
 @clients.command()
--- sales/clients/services.py
+++ sales/clients/services.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 from clients.models import Client
 
@@ -12,3 +13,31 @@ class ClientService:
         with open(self.table_name, mode='a') as f:
             writer = csv.DictWriter(f, fieldnames=Client.schema())
             writer.writerow(client.to_dict())
+
+
+    def list_clients(self):
+        with open(self.table_name, mode='r') as f:
+            reader = csv.DictReader(f, fieldnames=Client.schema())
+
+            return list(reader)
+
+    def update_client(self, updated_client):
+        clients = self.list_clients()
+
+        updated_clients = []
+        for client in clients:
+            if client['uid'] == updated_client.uid:
+                updated_clients.append(updated_client.to_dict())
+            else:
+                updated_clients.append(client)
+
+            self._save_to_disk(updated_clients)
+
+    def _save_to_disk(self, updated_clients):
+        tmp_table_name = self.table_name + '.tmp'
+        with open(tmp_table_name, mode='w') as f:
+            writer = csv.DictWriter(f, fieldnames=Client.schema())
+            writer.writerows(updated_clients)
+
+        os.remove(self.table_name)
+        os.rename(tmp_table_name, self.table_name)
